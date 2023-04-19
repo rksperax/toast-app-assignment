18cf61ddc2d2b54a67a82303026ac418831cbb0c
Rajan <rajan@sperax.io>
2023-04-19 19:42:58 +0530

    solved build error

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 
 //library
-import { BrowserRouter } from "react-router-dom";
+
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
 // components
 
-//@ts-ignore
 import App from "./components/App/App";
-//@ts-ignore
+
 import { store } from "./redux";
 
 // assets
 import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
