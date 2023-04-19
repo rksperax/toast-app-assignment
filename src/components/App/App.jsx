fbebe94ae0f80259535a34bc0202f523c23f0f45
Rajan <rajan@sperax.io>
2023-04-19 19:48:12 +0530

    added default redirect route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 // library
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 
 // components
 import { routes } from "./routes";
@@ -16,14 +16,13 @@ function App() {
     <div className={styles.app}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Special />} />
-
           <Route path={routes.main} element={<Main />} />
           <Route path={routes.special} element={<Special />} />
 
           <Route path={routes.placeOrder} element={<PlaceOrder />} />
           <Route path={routes.beverages} element={<Beverages />} />
           <Route path={routes.desserts} element={<Desserts />} />
+          <Route path="*" element={<Navigate to={routes.special} replace />} />
         </Routes>
       </BrowserRouter>
     </div>
